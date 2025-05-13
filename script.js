0fc776516c5a54903a53a0f5dd48911a65f4174f
Caroline <carolline.guimaraes10@gmail.com>
2025-05-13 16:50:38 -0300

    java js

---
+++ script.js
@@ -0,0 +1,29 @@
+window.onload = function(){
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(function(button) {
+button.addEventListener('click', function(){
+    const dia = this.getAttribute('data-dia');
+    
+    switch (dia) {
+                case "segunda":
+                    alert("Dia de começar a semana com energia!");
+                    break;
+                case "terça":
+                    alert("Hora de aprender coisas novas!");
+                    break;
+                case "quarta":
+                    alert("Metade da semana, continue firme!");
+                    break;
+                case "quinta":
+                    alert("Quase lá, um pouco mais de esforço!");
+                    break;
+                case "sexta":
+                    alert("Final de semana! Descanse e divirta-se!");
+                    break;
+                default:
+                    alert("Final de semana! Descanse e divirta-se!");
+            }
+}
+}
+}
